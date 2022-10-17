ae36d5c53f68f162b2d3bdcb6ade50b563e9fa9d
Наташа Ананас <konnat7@mail.ru>
2022-10-17 22:58:02 +0300

    Pattern Instagram

---
+++ Instagram_App/Instagram_App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  Instagram_App
+//
+//  Created by Анастасия Козлова on 17.10.2022.
+//
+
+import UIKit
+
+/// SceneDelegate
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
---
+++ Instagram_App/Instagram_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_App
+//
+//  Created by Анастасия Козлова on 17.10.2022.
+//
+
+import UIKit
+
+/// ViewController
+final class ViewController: UIViewController {
+
+    // MARK: - Private Visual Components
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    // MARK: - Private Methods
+}

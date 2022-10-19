ad121808bbfa1a4eb591a3e954bb2a65b261894a
Наташа Ананас <konnat7@mail.ru>
2022-10-19 09:49:03 +0300

    Выполнила домашние задание 25

---
+++ Instagram_App/Instagram_App/Controllers/AddViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddViewController.swift
+//  Instagram_App
+//
+//  Created by Анастасия Козлова on 17.10.2022.
+//
+
+import UIKit
+
+/// Добавление новой публикации
+final class AddViewController: UIViewController {
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Instagram_App/Instagram_App/Controllers/LikeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LikeViewController.swift
+//  Instagram_App
+//
+//  Created by Анастасия Козлова on 17.10.2022.
+//
+
+import UIKit
+
+/// Уведомления пользователя
+final class LikeViewController: UIViewController {
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
